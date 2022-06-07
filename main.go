6b1931b7c8ee4b4b33dedf186714e1497f1dfacd
elout-project <hidayat.mudha@gmail.com>
2022-06-07 21:30:04 +0700

    adding gorilla/mux

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"net/http"
 	"web-app/handler"
-	_"web-app/salah"
+	_ "web-app/salah"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	// Routing
-	http.HandleFunc("/", handler.Index)
-	http.HandleFunc("/about", handler.About)
-	// http.HandleFunc("/salah", salah.Salah)
+	// // Routing
+	// http.HandleFunc("/", handler.Index)
+	// http.HandleFunc("/about", handler.About)
+	// // http.HandleFunc("/salah", salah.Salah)
+	// // Start Server
+	// http.ListenAndServe(":80", nil)
 
-	// Start Server
-	http.ListenAndServe(":80", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler.Index)
+	r.HandleFunc("/about", handler.About)
+	http.ListenAndServe(":80", r)
 }
