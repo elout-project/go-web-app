4b238ae063e59e0319b268533a8b9f739c7142c7
elout-project <hidayat.mudha@gmail.com>
2022-06-07 21:05:16 +0700

    drop route salah

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"net/http"
 	"web-app/handler"
-	"web-app/salah"
+	_"web-app/salah"
 )
 
 func main() {
 	// Routing
 	http.HandleFunc("/", handler.Index)
 	http.HandleFunc("/about", handler.About)
-	http.HandleFunc("/salah", salah.Salah)
+	// http.HandleFunc("/salah", salah.Salah)
 
 	// Start Server
 	http.ListenAndServe(":80", nil)
